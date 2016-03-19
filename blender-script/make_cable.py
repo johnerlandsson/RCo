6d5960df2a4049317524b3a156f7491b9246d1eb
John Erlandsson <john@lunatech.se>
2016-03-19 22:34:21 +0100

    Started working on conductors

--- blender-script/make_cable.py
+++ blender-script/make_cable.py
@@ -1,88 +1,155 @@
 import bpy
 import math
 
-def make_circle(radius):
-    n_points = 32
-    curveData = bpy.data.curves.new('Circle', type = 'CURVE')
+WORK_LAYER = [False, True, False, False, False, False, False, 
+              False, False, False, False, False, False, False, 
+              False, False, False, False, False, False]
+JUNK_LAYER = [False, False, True, False, False, False, False, 
+              False, False, False, False, False, False, False, 
+              False, False, False, False, False, False]
+
+# make_line
+# Returns a line segment
+# p1: First point of line segment
+# p2: Second point of line segment
+def make_line(p1, p2):
+    curveData = bpy.data.curves.new('Line', type = 'CURVE')
     curveData.dimensions = '3D'
-    curveData.resolution_u = 12
-    polyline = curveData.splines.new('NURBS')
-    curveData.splines[0].use_cyclic_u = True
-    polyline.points.add(n_points + 1)
+    #curveData.resolution_u = 12
     
-    theta = 0.0
-    for i in range(n_points + 1):
-        x = radius * math.cos(theta)
-        y = radius * math.sin(theta)
-        z = 0
-        
-        polyline.points[i].co = (x, y, z, 1)
-        
-        theta += (2.0 * math.pi) / n_points
-        
+    objectData = bpy.data.objects.new("objLine", curveData)
+    objectData.location = (0, 0, 0)
+    objectData.data.use_fill_caps = True
+    bpy.context.scene.objects.link(objectData)
     
-    return bpy.data.objects.new('Circle', curveData)
+    polyline = curveData.splines.new('POLY')
+    polyline.points.add(2)
+    polyline.points[0].co = (p1[0], p1[1], p1[2], 1)
+    polyline.points[0].co = (p2[0], p2[1], p2[2], 1)
+    
+    return objectData
+ 
+# about_eq
+# Helper function to compare floating point values
+def about_eq(a, b):
+    if a + 0.0001 > b and a - 0.0001 < b:
+        return True
+    return False
 
-def make_spiral(length, pitch, radius, start_angle = 0.0, points_per_revolution = 32):
+# make_bezier_spiral
+# Returns a bezier curve in the shape of a spiral
+# length: Total length in Z-axis
+# pitch: Number of revolutions per length unit
+# radius: Radius of spiral
+def make_bezier_spiral(length, pitch, radius):
     #Calculate limits
-    max_theta = length * pitch * 2.0 * math.pi
-    n_points = 2 + int(max_theta / ((2.0 * math.pi) / points_per_revolution))
-
-    #Create a NURBS curve ovject
+    n_points = int(length * pitch * 4.0)
+    
+    #Create a Bezier curve object
     curveData = bpy.data.curves.new('Spiral', type = 'CURVE')
     curveData.dimensions = '3D'
-    curveData.resolution_u = 12
-    polyline = curveData.splines.new('NURBS')
-    polyline.points.add(n_points)
-
+    curveData.resolution_u = 0
+    curveData.use_fill_caps = True  
+    polyline = curveData.splines.new('BEZIER')
+    polyline.bezier_points.add(n_points)
+    
     #Create points
     theta = 0.0
-    for i in range(n_points):
+    for i in range(n_points + 1):
         z = length * (i / n_points)
-        x = radius * math.cos(theta + start_angle)
-        y = radius * math.sin(theta + start_angle)
+        x = radius * math.cos(theta)
+        y = radius * math.sin(theta)
+        polyline.bezier_points[i].co = (x, y, z)
         
-        polyline.points[i].co = (x, y, z, 1)
+        if about_eq(x, 0.0) and about_eq(y, radius):
+            lhy = y
+            rhy = y
+            lhx = (radius / 2.0)
+            rhx = -(radius / 2.0)
+        if about_eq(x, 0.0) and about_eq(y, -radius):
+            lhy = y
+            rhy = y
+            lhx = -(radius / 2.0)
+            rhx = (radius / 2.0)
+        if about_eq(x, radius) and about_eq(y, 0.0):
+            lhx = x
+            rhx = x
+            lhy = -(radius / 2.0)
+            rhy = (radius / 2.0)
+        if about_eq(x, -radius) and about_eq(y, 0.0):
+            lhx = x
+            rhx = x
+            lhy = (radius / 2.0)
+            rhy = -(radius / 2.0)
+               
+               
+        dhz = (length / n_points) / 4.0 
+        lhz = z - dhz
+        rhz = z + dhz
+    
+        polyline.bezier_points[i].handle_left = (lhx, lhy, lhz)
+        polyline.bezier_points[i].handle_right = (rhx, rhy, rhz)
+    
+        polyline.bezier_points[i].handle_left_type = 'ALIGNED'
+        polyline.bezier_points[i].handle_left_type = 'ALIGNED'
         
-        theta += (2.0 * math.pi) / points_per_revolution
+        print("%f, %f, %f" %(x, y, z))
+        
+        theta += (2.0 * math.pi) / 4.0
 
     #Add object to a scene
     return bpy.data.objects.new('Spiral', curveData)
 
-def rotation_normal_to_spiral(spiral):
-    if len(spiral.data.splines[0].points) < 2:
-        print("rotation_normal_to_spiral: Not enough points in spiral.")
-        return None
+def strand_positions(conductor_radius, strand_radius):
+    ret = []
+
+    while True:
+        no = int(math.floor((2.0 * math.pi * conductor_radius) / (2.0 * strand_radius)))
+        ps = []
+        x0 = conductor_radius * math.cos( 0 * 2 * math.pi / no)
+        y0 = conductor_radius * math.sin( 0 * 2 * math.pi / no)
+        x1 = conductor_radius * math.cos( 1 * 2 * math.pi / no)
+        y1 = conductor_radius * math.sin( 1 * 2 * math.pi / no)
+        dist = math.sqrt((x0 - x1)**2 + (y0 - y1)**2)
+
+        if dist < 2.0 * strand_radius:
+            no -= 1
+
+        for i in range(0, no):
+            x = conductor_radius * math.cos(i * 2 * math.pi / no)
+            y = conductor_radius * math.sin(i * 2 * math.pi / no)
+            ps.append((x, y))
+
+        ret.append(ps)
+
+        rc_next = conductor_radius - (2.0 * strand_radius)
+        if rc_next < strand_radius:
+            if conductor_radius > 2.0 * strand_radius:
+                ret.append([(0, 0)])
+            break
+        else:
+            conductor_radius = rc_next
+
+    return ret
+
+def make_solid_conductor(length, radius):
+    line = make_line((0, 0, 0), (0, 0, length))
+    bpy.ops.curve.primitive_bezier_circle_add(location = (0, 0, 0), layers = JUNK_LAYER)
+    circle = bpy.context.active_object
+    circle.scale = (radius, radius, 0)
     
-    p1 = spiral.data.splines[0].points[0].co[0:3]
-    p2 = spiral.data.splines[0].points[1].co[0:3]
+    line.data.bevel_object = circle
+    line.data.use_fill_caps = True
     
-    rx = 1/math.acos(p1[0] - p2[0])
-    ry = 1/math.asin(p1[1] - p2[1])
-    rz = 1/math.asin(p1[2] - p2[2])
-    print("%f, %f, %f" %(p1[0]-p2[0], p1[1]-p2[1], p1[2]-p2[2]))
-    print("%f, %f, %f" %(rx, ry, rz))
+     
     
-    return (rx, ry, rz)
 
-def draw_spiral_part(length, part_radius, pitch, pitch_radius, start_angle): 
-    spiral = make_spiral(length, pitch, pitch_radius, start_angle)
-    scn = bpy.context.scene
-    scn.objects.link(spiral)
-    scn.objects.active = spiral
-
-    circle = make_circle(part_radius)
-    scn.objects.link(circle)
-    scn.objects.active = circle
-    circle.rotation_mode = 'XYZ'
-    print(type(circle))
-    #circle.rotation_euler = rotation_normal_to_spiral(spiral)
-    circle.location = (pitch_radius, 0, 0)
-
-    spiral.data.bevel_object = circle
-
-for i in range(6):
-    theta = i * ((2.0 * math.pi) / 6)
-    draw_spiral_part(3.0, 0.05, 0.5, 0.12, theta)
-
-#print("%f, %f, %f" %(rot[0], rot[1], rot[2]))
+def make_conductor(length, conductor_radius, strand_radius):
+    if conductor_radius == strand_radius or strand_radius == None:
+        return make_solid_conductor(length, conductor_radius)
+    
+    return make_stranded_conductor(length, conductor_radius, strand_radius)        
+        
+        
+        
+make_conductor(3.0, 0.00162 / 2.0, None)
